0b1eacfa587157e0065219aff9f4de88e795a76a
prajwalshahane108 <prajwal@dshgsonic.com>
2025-04-10 18:53:29 +0530

    es lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.js
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/jsx-key": "off"
+    }
+  }
+];
 
-// export default eslintConfig;
+export default eslintConfig;
